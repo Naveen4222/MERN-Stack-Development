e1d1294cb6b1ca253811b879579e318a68de9992
Naveen4222 <naveen7082796682@gmail.com>
2026-06-09 17:57:19 +0530

    cors install to connect backend and frontend

--- server/server.js
+++ server/server.js
@@ -2,14 +2,24 @@ import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 import userRoute from "./router/auth-router.js";
-
 import { connectDb } from "./utils/db.js";
 import { errorMiddleware } from "./middleware/error-middleware.js";
 import contactRouter from "./router/contact-router.js";
+import cors from "cors";
 
 
-const app = express();
-const PORT = 5000;
+const app = express(); 
+
+const corsOptions = {
+        origin:"http://localhost:5173",
+        method:"POST, GET, PUT, DELETE PATCH HEAD",
+        credentials:true
+
+}
+// lets tackel cors
+app.use(cors(corsOptions));
+
+const PORT = process.env.PORT;
 app.use(express.json());
 
 // app.use(express.json()) : This line of code adds Express middleware that parses incoming request bodies with JSON payloads. It's important to  place this before any routes that need to handle JSON data in the request body. This middleware is responsible for parsing JSON data from requrest, and it should be applied at the beginning of your middleware stack to ensure it's available for all subsequent route handlers.
