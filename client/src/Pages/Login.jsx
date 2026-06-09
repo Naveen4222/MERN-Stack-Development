a33783ed15dc61ca32b2bfc25ee1a468043c569f
Naveen4222 <naveen7082796682@gmail.com>
2026-06-09 11:50:07 +0530

    Handle Login From

--- client/src/Pages/Login.jsx
+++ client/src/Pages/Login.jsx
@@ -1,7 +1,43 @@
+import { useState } from "react"
+
 export const Login = ()=>{
+
+    const [user,setUser] = useState({
+        email:"",
+        password:""
+    })
+
+    const handleInputChange = (e)=>{
+        let name = e.target.name;
+        let value = e.target.value;
+
+        setUser({
+            ...user,
+            [name]:value
+        })
+
+    }
+
+    const handleFormSubmit =(e)=>{
+        event.preventDefault(e);
+        console.log(user);
+    }
+
     return (
         <>
-        <h1>This is Login Page</h1>
+       <div>
+        <div>
+        <form onSubmit={handleFormSubmit}>
+            <label htmlFor="email">Email</label>
+            <input type="email" name="email" placeholder="Enter your email" value={user.email} onChange={handleInputChange} />
+             <label htmlFor="password">Password</label>
+            <input type="password" name="password" placeholder="Enter your password" value={user.password} onChange={handleInputChange}/>
+        </form>
+        </div>
+        <div></div>
+        <button type="submit" onClick={handleFormSubmit}>Login</button>
+
+       </div>
         </>
     )
 }
