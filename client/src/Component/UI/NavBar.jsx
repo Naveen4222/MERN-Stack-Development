1a706d73fa56f18d679b1fbee4ed8cf54af0986b
Naveen4222 <naveen7082796682@gmail.com>
2026-06-12 10:36:58 +0530

    login register and logout toggel tackel

--- client/src/Component/UI/NavBar.jsx
+++ client/src/Component/UI/NavBar.jsx
@@ -1,6 +1,10 @@
 import { NavLink } from "react-router-dom";
 import './NavBar.css'
+import { useAuth } from "../../store/auth";
+
 export const NavBar = () => {
+
+    const { isLogIn } = useAuth();
     return (
         <>
             <header>
@@ -11,12 +15,32 @@ export const NavBar = () => {
 
                     <nav >
                         <ul >
-                            <li><NavLink to="/">HOME</NavLink></li>
-                            <li><NavLink to="/about">ABOUT</NavLink></li>
-                            <li><NavLink to="/service">SERVICE</NavLink></li>
-                            <li><NavLink to="/contact">CONTACT US</NavLink></li>
-                            <li><NavLink to="/register">REGISTER</NavLink></li>
-                            <li><NavLink to="/login">LOGIN</NavLink></li>
+                            <li>
+                                <NavLink to="/">HOME</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/about">ABOUT</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/service">SERVICE</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/contact">CONTACT US</NavLink>
+                            </li>
+                            {isLogIn ? (<li>
+                                <NavLink to="/logout">Logout</NavLink>
+                            </li> ):
+                                <>
+
+                                    <li>
+                                        <NavLink to="/register">REGISTER</NavLink>
+                                    </li>
+                                    <li>
+                                        <NavLink to="/login">LOGIN</NavLink>
+                                    </li>
+                                </>
+                            }
+
 
                         </ul>
                     </nav>
