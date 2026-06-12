8011038026aa91aa84f0a4d6ca1c034693c2706c
Naveen4222 <naveen7082796682@gmail.com>
2026-06-12 12:53:42 +0530

    JWT Authenctication

--- client/src/store/auth.jsx
+++ client/src/store/auth.jsx
@@ -1,19 +1,23 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 export const AuthContext = createContext()
 
 
 export const AuthProvider = ({ children }) => {
 
-    const [token, setToken] = useState(localStorage.getItem('token'))
+    const [token, setToken] = useState(localStorage.getItem('token'));
+    const [user, setUser] = useState("");
+
+
     const storeTokenInLS = (serverToken) => {
-        localStorage.setItem("token", serverToken);
         setToken(serverToken);
+        return localStorage.setItem("token", serverToken);
+        
 
     }
 
     let isLogIn = !!token;
-    console.log('isLogIn',isLogIn);
+    console.log('isLogIn', isLogIn);
 
 
     const LogoutUser = () => {
@@ -22,8 +26,40 @@ export const AuthProvider = ({ children }) => {
 
     };
 
+    //JWT Authentication - currently login user data
+    const useAuthentication = async () => {
+        try {
+            
+            const response = await fetch(`http://localhost:5000/user`, {
+                method: "GET",
+                headers: {
+                    Authorization: `Bearer ${token}`
+                },
+            });
+            if (response.ok) {
+                const data = await response.json();
+                console.log("user data", data.userData);
+                setUser(data.userData);
+                
 
-    return <AuthContext.Provider value={{ storeTokenInLS, LogoutUser , isLogIn }}>
+            } 
+
+
+        }
+        catch (error) {
+            console.log(error)
+        }
+
+    }
+
+    useEffect(() => {
+        useAuthentication();
+    }, [])
+
+
+
+
+    return <AuthContext.Provider value={{ storeTokenInLS, LogoutUser, isLogIn ,user}}>
         {children}
     </AuthContext.Provider>
 
