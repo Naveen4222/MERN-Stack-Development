2e36ba7f23a31a37f30bdee4fc9f650eeba6f1d4
Naveen4222 <naveen7082796682@gmail.com>
2026-06-12 13:12:06 +0530

    inital Commit

--- client/src/Pages/About.jsx
+++ client/src/Pages/About.jsx
@@ -1,7 +1,12 @@
-export const About = ()=>{
-    return(
+import { useAuth } from "../store/auth";
+export const About = () => {
+    const {user} = useAuth();
+    console.log(user);
+    console.log(user.username);   
+    return (
         <>
-        <h1>This is About Page</h1>
+            <p>Welcome, {user?user.username:"Welcome to our website"}</p>
+            <h1>This is About Page</h1>
         </>
     )
 }
